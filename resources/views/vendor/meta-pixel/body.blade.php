296762251c07b301e64a231d10dc6fe301540218
anjas putra <anjasputra012@gmail.com>
2026-02-23 13:18:23 +0700

    'meta'

--- resources/views/vendor/meta-pixel/body.blade.php
+++ resources/views/vendor/meta-pixel/body.blade.php
@@ -1,32 +1,51 @@
-@if($metaPixel->isEnabled())
-@unless(empty($eventLayer))
-<!-- Meta Pixel Events -->
-<script>
-@foreach($eventLayer as $eventName => $metaPixel)
-@if(empty($metaPixel['event_id']) && empty($metaPixel['data']))
-    fbq('track', '{{ $eventName }}');
-@elseif(empty($metaPixel['event_id']))
-    fbq('track', '{{ $eventName }}', {{ Js::from($metaPixel['data']) }});
-@else
-    fbq('track', '{{ $eventName }}', {{ Js::from($metaPixel['data']) }}, {eventID: '{{ $metaPixel['event_id'] }}'});
-@endif
-@endforeach
-</script>
-<!-- End Meta Pixel Events -->
-@endunless
-@unless(empty($customEventLayer))
-<!-- Meta Pixel Custom Events -->
-<script>
-@foreach($customEventLayer as $customEventName => $metaPixel)
-@if(empty($metaPixel['event_id']) && empty($metaPixel['data']))
-   fbq('trackCustom', '{{ $customEventName }}');
-@elseif(empty($metaPixel['event_id']))
-    fbq('trackCustom', '{{ $customEventName }}', {{ Js::from($metaPixel['data']) }});
-@else
-   fbq('trackCustom', '{{ $customEventName }}', {{ Js::from($metaPixel['data']) }}, {eventID: '{{ $metaPixel['event_id'] }}'});
-@endif
-@endforeach
-</script>
-<!-- End Meta Custom Pixel Events -->
-@endunless
+@if ($metaPixel->isEnabled())
+    @unless (empty($eventLayer) && empty($customEventLayer))
+        <script>
+            (function() {
+                function fireMetaPixelEvents() {
+                    if (typeof fbq === 'undefined') return;
+
+                    {{-- Standard Events --}}
+                    @foreach ($eventLayer ?? [] as $eventName => $metaPixel)
+                        @if (empty($metaPixel['event_id']) && empty($metaPixel['data']))
+                            fbq('track', '{{ $eventName }}');
+                        @elseif (empty($metaPixel['event_id']))
+                            fbq('track', '{{ $eventName }}', {{ Js::from($metaPixel['data']) }});
+                        @else
+                            fbq(
+                                'track',
+                                '{{ $eventName }}',
+                                {{ Js::from($metaPixel['data']) }}, {
+                                    eventID: '{{ $metaPixel['event_id'] }}'
+                                }
+                            );
+                        @endif
+                    @endforeach
+
+                    {{-- Custom Events --}}
+                    @foreach ($customEventLayer ?? [] as $customEventName => $metaPixel)
+                        @if (empty($metaPixel['event_id']) && empty($metaPixel['data']))
+                            fbq('trackCustom', '{{ $customEventName }}');
+                        @elseif (empty($metaPixel['event_id']))
+                            fbq('trackCustom', '{{ $customEventName }}', {{ Js::from($metaPixel['data']) }});
+                        @else
+                            fbq(
+                                'trackCustom',
+                                '{{ $customEventName }}',
+                                {{ Js::from($metaPixel['data']) }}, {
+                                    eventID: '{{ $metaPixel['event_id'] }}'
+                                }
+                            );
+                        @endif
+                    @endforeach
+                }
+
+                // 1️⃣ initial page load
+                fireMetaPixelEvents();
+
+                // 2️⃣ Livewire SPA navigation
+                document.addEventListener('livewire:navigated', fireMetaPixelEvents);
+            })();
+        </script>
+    @endunless
 @endif
